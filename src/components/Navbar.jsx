73f59fac8c6825d951733741f9e0f25dc85b6ed8
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-20 15:39:07 +0530

    setup done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,12 @@ export default function Navbar() {
   const movies = useSelector((state) => state.moviesList.value);
   return (
     <div>
-      <h1>
-        Navbar here:
-        {movies.length > 0 ? <li>{movies[1].name} </li> : <li>none</li>}
-      </h1>
+      
+     {movies.map((movie)=>{
+      return <li>{movie.name}</li>
+     })}   
+           
+    
     </div>
-  );
+  )
 }
